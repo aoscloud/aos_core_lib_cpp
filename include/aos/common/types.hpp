5207fd873c9381fb56b8784275e10de271c3d16d
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2023-03-13 17:48:57 +0200

    [common,types] Add ServiceInfo and LayerInfo structures
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- include/aos/common/types.hpp
+++ include/aos/common/types.hpp
@@ -17,6 +17,11 @@
 namespace aos {
 
 /*
+ * Provider ID len.
+ */
+constexpr auto cProviderIDLen = AOS_CONFIG_TYPES_PROVIDER_ID_LEN;
+
+/*
  * Service ID len.
  */
 constexpr auto cServiceIDLen = AOS_CONFIG_TYPES_SERVICE_ID_LEN;
@@ -27,6 +32,16 @@ constexpr auto cServiceIDLen = AOS_CONFIG_TYPES_SERVICE_ID_LEN;
 constexpr auto cSubjectIDLen = AOS_CONFIG_TYPES_SUBJECT_ID_LEN;
 
 /*
+ * Layer ID len.
+ */
+constexpr auto cLayerIDLen = AOS_CONFIG_TYPES_LAYER_ID_LEN;
+
+/*
+ * Layer digest len.
+ */
+constexpr auto cLayerDigestLen = AOS_CONFIG_TYPES_LAYER_DIGEST_LEN;
+
+/*
  * Instance ID len.
  */
 constexpr auto cInstanceIDLen = AOS_CONFIG_TYPES_INSTANCE_ID_LEN;
@@ -35,6 +50,32 @@ constexpr auto cInstanceIDLen = AOS_CONFIG_TYPES_INSTANCE_ID_LEN;
  * File path len.
  */
 constexpr auto cFilePathLen = AOS_CONFIG_TYPES_FILE_PATH_LEN;
+
+/*
+ * URL len.
+ */
+constexpr auto cURLLen = AOS_CONFIG_TYPES_URL_LEN;
+
+/**
+ * Vendor version len.
+ */
+constexpr auto cVendorVersionLen = AOS_CONFIG_TYPES_VENDOR_VERSION_LEN;
+
+/**
+ * Service/layer description len.
+ */
+
+constexpr auto cDescriptionLen = AOS_CONFIG_TYPES_DESCRIPTION_LEN;
+
+/**
+ * SHA256 size.
+ */
+constexpr auto cSHA256Size = 32;
+
+/**
+ * SHA512 size.
+ */
+constexpr auto cSHA512Size = 64;
 
 /**
  * Instance identification.
@@ -82,6 +123,45 @@ struct InstanceStatus {
     uint64_t         mAosVersion;
     InstanceRunState mRunState;
     Error            mError;
+};
+
+/**
+ * Version info.
+ */
+struct VersionInfo {
+    uint64_t mAosVersion;
+    char     mVendorVersion[cVendorVersionLen + 1];
+    char     mDescription[cDescriptionLen + 1];
+};
+
+/**
+ * Service info.
+ */
+
+struct ServiceInfo {
+    VersionInfo mVersionInfo;
+    char        mServiceID[cServiceIDLen + 1];
+    char        mProviderID[cProviderIDLen + 1];
+    uint32_t    mGID;
+    char        mURL[cURLLen + 1];
+    uint8_t     mSHA256[cSHA256Size];
+    uint8_t     mSHA512[cSHA512Size];
+    size_t      mSize;
+};
+
+/**
+ * Layer info.
+ */
+
+// LayerInfo layer info.
+struct LayerInfo {
+    VersionInfo mVersionInfo;
+    char        mLayerID[cLayerIDLen + 1];
+    char        mLayerDigest[cLayerDigestLen + 1];
+    char        mURL[cURLLen + 1];
+    uint8_t     mSHA256[cSHA256Size];
+    uint8_t     mSHA512[cSHA512Size];
+    size_t      mSize;
 };
 
 } // namespace aos
